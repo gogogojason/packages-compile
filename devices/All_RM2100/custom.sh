e7805dcb020ea1e3471b973ad3223b3a69e4e43c
icons <65128346+gogogojason@users.noreply.github.com>
2021-03-10 12:59:11 +0800

    Update custom.sh

--- devices/All_RM2100/custom.sh
+++ devices/All_RM2100/custom.sh
@@ -1,9 +1,8 @@
-#rm -Rf package/feeds/packages/node
-#svn co https://github.com/coolsnowwolf/packages/trunk/lang/node package/feeds/custom/node
+#!/bin/bash
+
 sed -i 's/${Arch}/${Arch}_softfloat/g' feeds/custom/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
 rm -f feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
 rm -f feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
 cp package/logos/milogo.png feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
 cp package/logos/opunblockmusic.po feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
-cp package/logos/opcpuinfo files/sbin/cpuinfo
 rm -Rf package/logos
